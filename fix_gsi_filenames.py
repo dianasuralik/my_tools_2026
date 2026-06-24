b18f92972f8709ebdc68cb184a84af3675261cbb
dianasuralik <diana.suralik@noaa.gov>
2026-06-24 10:25:10 -0400

    Update fix_gsi_filenames.py

--- fix_gsi_filenames.py
+++ fix_gsi_filenames.py
@@ -1,11 +1,12 @@
 #-------------------------------------------------------
 ### start fix_gsi_filenames.py
 
-#!/usr/bin/env python3
 """
 Generic script extracts tar, creates .nc symlinks pointing to .gsi.nc files, rebuilds tar, and writes the result to a specified output directory. 
 The original archive is not changed. v3
 """
+
+#!/usr/bin/env python3
 
 ### imports
 import os
